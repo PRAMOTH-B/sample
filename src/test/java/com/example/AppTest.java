0739a4384c5bd556aa8acfb05a177ceeb248e9c0
PRAMOTH BALAKUMAR <128211286+PRAMOTH-B@users.noreply.github.com>
2024-08-15 16:00:00 +0530

    Update AppTest.java

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,38 +1,11 @@
-package com.example;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    TestClass1.class,
+    TestClass2.class,
+    // add more test classes here
+})
+public class RegressionTestSuite {
 }
